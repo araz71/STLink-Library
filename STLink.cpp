bcd4df822949b4a8e6facda64a80bf28273ccced
Amin Aghakhani <amin.aghakhani71@gmail.com>
2023-12-22 10:55:16 -0800

    Under developing

---
+++ STLink.cpp
@@ -0,0 +1,100 @@
+#include "STLink.h"
+
+using namespace Jeyran;
+
+bool STLink::wait_for_command(int msec = 5000) {
+    if (!proc.waitForStarted(msec)) {
+        return false;
+    }
+
+    if (!proc.waitForReadyRead(msec)) {
+        return false;
+    }
+
+    return true;
+}
+STLink::STLink() {
+    connection_state = false;
+
+    proc.start("ST-LINK_CLI.exe", {"-c", "swd"});
+    if (wait_for_command()) {
+        std::string result = proc.readAll().toStdString();
+        if (result.find_first_of("No ST-LINK detected!") != std::string::npos) {
+            throw std::domain_error("No ST-Link detected");
+        }
+    } else {
+        throw std::domain_error("ST-Link_CLI calling failed.");
+    }
+
+    proc.close();
+}
+
+void STLink::program(QString filename) {
+    QStringList args = {"-c", "swd", "UR", "-P", filename};
+
+    proc.start("ST-LINK_CLI.exe", args);
+    if (wait_for_command()) {
+        QByteArray result = proc.readAll();
+        // TODO
+    }
+
+    proc.close();
+}
+
+bool STLink::set_read_protection(ProtectionLevel level)
+{
+    proc.start("ST-LINK_CLI.exe", {"-c", "swd", "-OB", "RDP", QString::number(level)});
+    if (wait_for_command()) {
+        proc.close();
+        return true;
+    }
+
+    proc.close();
+    return false;
+}
+
+bool STLink::erase() {
+    bool ret_value = false;
+
+    proc.start("ST-LINK_CLI.exe", {"-c", "swd", "-ME"});
+    if (wait_for_command()) {
+        std::string result = proc.readAll().toStdString();
+        if (result.find_first_of("Flash memory erased.") != std::string::npos) {
+            ret_value = true;
+        }
+    }
+
+    proc.close();
+    return ret_value;
+}
+
+std::map<STLink::ConnectionInformation, std::string> STLink::get_connection_information(bool& ok)
+{
+    std::map<STLink::ConnectionInformation, std::string> return_result;
+    ok = true;
+    proc.start("ST-LINK_CLI.exe", {"-c", "swd"});
+    if (wait_for_command()) {
+        std::string result = proc.readAll().toStdString();
+        while (result.find_first_of("\n") != std::string::npos) {
+            auto line = result.substr(0, result.find_first_of("\n"));
+            result = result.substr(result.find_first_of("\n") + 1, result.size());
+
+            if (line.find_first_of(":") != std::string::npos) {
+                auto key = line.substr(0, line.find_first_of(":"));
+                auto value = line.substr(line.find_first_of(":") + 2, line.size());
+                if (key == "Device family") {
+                    return_result[STLink::ConnectionInformation::DeviceFamily] = value;
+                } else if (key == "Device flash Size") {
+                    return_result[STLink::ConnectionInformation::DeviceFlashSize] = value;
+                } else if (key == "Device ID") {
+                    return_result[STLink::ConnectionInformation::DeviceID] = value;
+                }
+            }
+        }
+    } else {
+        ok = false;
+    }
+    proc.close();
+    return return_result;
+}
+
---
+++ STLink.h
@@ -0,0 +1,48 @@
+#ifndef STLINK_H
+#define STLINK_H
+
+#include <QObject>
+#include <QProcess>
+#include <map>
+#include <string>
+
+namespace Jeyran
+{
+
+class STLink
+{
+    QProcess proc;
+    bool connection_state;
+
+    std::map<std::string, std::string> informations;
+
+public:
+    enum ConnectionMode{
+        SWD,
+        JTAG
+    };
+
+    enum ProtectionLevel {
+        NoProtection,
+        Level1,
+        Level2,
+    };
+
+    enum ConnectionInformation {
+        DeviceFamily,
+        DeviceID,
+        DeviceFlashSize,
+        STLinkSerialNumber,
+    };
+
+    explicit STLink();
+
+    void program(QString filename);
+    bool set_read_protection(STLink::ProtectionLevel level);
+    bool erase();
+    std::map<STLink::ConnectionInformation, std::string> get_connection_information(bool &ok);
+    bool wait_for_command(int msec);
+};
+
+}
+#endif // STLINK_H
